da434336c1a3499369cca22a20d90438a88df79b
Dominik <dominikhruza92@gmail.com>
2020-08-27 10:49:06 +0100

    db uri

--- config/db_config.js
+++ config/db_config.js
@@ -3,10 +3,13 @@ const url = process.env.MONGO_URI || "mongodb://localhost:27017/movies";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://movies:movies@cluster0.8ieie.mongodb.net/movies_db?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Database connected...");
   } catch (error) {
     console.error(error.message);
